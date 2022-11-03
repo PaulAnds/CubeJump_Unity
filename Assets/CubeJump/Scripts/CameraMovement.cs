be2ccb1e40fb5ed329b4b763f565074bf221a23e
PaulAnds <paulandressolis@gmail.com>
2022-11-03 01:47:47 -0600

    removed shaders

--- Assets/CubeJump/Scripts/CameraMovement.cs
+++ Assets/CubeJump/Scripts/CameraMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform target;
+    public GameObject target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player");
+    }
 
     void FixedUpdate()
     {
         if (target)
         {
-            Vector3 desiredPosition = new Vector3(0, target.position.y, target.position.z) + offset;
+            Vector3 desiredPosition = new Vector3(0, target.transform.position.y, target.transform.position.z) + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
         }
